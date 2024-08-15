0a1e1a10e41cfb9bd089363f1cb5e996e139bb29
Leo Tseng <leo81xi16@gmail.com>
2024-08-15 11:11:25 +0800

    build: 設定 view engine & 使用教案HTML

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ const port = 3000
 
 app.use(express.static('public'))
 
+app.engine('.hbs', engine({extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+
 app.get('/', (req, res)=>{
   res.redirect('/restaurants')
 })
 
 app.get('/restaurants', (req, res)=>{
-  res.send('restaurants list')
+  res.render('index')
 })
 
 app.get('/restaurant/:id', (req, res)=>{
